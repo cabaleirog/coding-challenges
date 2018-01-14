be64de80c30bf3b88afe31ae56dde6899ed472e8
Paolo Cabaleiro <cabaleirog@gmail.com>
2018-01-13 21:47:49 -0300

    Adding solution for problem Subset with unique sum

---
+++ hacker_rank/subsets_with_a_unique_sum.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 -*-
+"""Project Euler #201: Subsets with a unique sum.
+
+https://www.hackerrank.com/contests/projecteuler/challenges/euler201/problem
+
+
+For any set A of numbers, let sum(A) be the sum of the elements of A.
+
+Consider the set B = {1,3,6,8,10,11}. There are 20 subsets of B containing
+three elements, and their sums are:
+
+    sum({1, 3, 6})       = 10
+    sum({1, 3, 8})       = 12
+    sum({1, 3, 10})      = 14
+    sum({1, 3, 11})      = 15
+    sum({1, 6, 8})       = 15
+    sum({1, 6, 10})      = 17
+    sum({1, 6, 11})      = 18
+    sum({1, 8, 10})      = 19
+    sum({1, 8, 11})      = 20
+    sum({1, 10, 11})     = 22
+    sum({3, 6, 8})       = 17
+    sum({3, 6, 10})      = 19
+    sum({3, 6, 11})      = 20
+    sum({3, 8, 10})      = 21
+    sum({3, 8, 11})      = 22
+    sum({3, 10, 11})     = 24
+    sum({6, 8, 10})      = 24
+    sum({6, 8, 11})      = 25
+    sum({6, 10, 11})     = 27
+    sum({8, 10, 11})     = 29
+
+Some of these sums occur more than once, others are unique. For a set A,
+let U(A,k) be the set of unique sums of k-element subsets of A, in our
+example we find U(B,3) = {10,12,14,18,21,25,27,29} and sum(U(B,3)) = 156.
+
+Now consider the n-element set S = {s1,s2,...,sn}. S has nCm m-element
+subsets. Determine the sum of all integers which are the sum of exactly
+one of the m-element subsets of S, i.e. find sum(U(S,m)).
+"""
+from itertools import combinations
+
+
+def preprocess_data(numbers, size):
+    """Remove unnecessary duplicates.
+
+    Args:
+        numbers (list): The set of numbers
+        size (int): Size of the subsets
+
+    Returns:
+        list: The original numbers list or a subset of it
+    """
+    if size <= 2:
+        return numbers, [], []
+
+    times = {}
+    for value in numbers:
+        if value in times:
+            times[value] += 1
+        else:
+            times[value] = 1
+
+    # Remove any duplicates with lenght equals to group size, but store the
+    # the set of the element with the size of group size
+    force_ignore = [x[0] for x in times.items() if x[1] == size]
+    extra_groups = [size * (x,) for x in force_ignore]
+    force_ignore.extend([x[0] for x in times.items() if x[1] > size])
+
+    to_include = list(filter(lambda x: x[1] < size, times.items()))
+    result = []
+    for item in to_include:
+        result.extend([item[0],] * item[1])
+
+    # TODO: Try to find another approach, it can be excluded until only 1 but
+    # looks like only once will not always work
+    for item in force_ignore:
+        result.extend([item,] * 1)
+    return result, force_ignore, extra_groups
+
+
+def get_sum(numbers, size, reduce_array=True):
+    """Return the sum of all unique sums of the subsets.
+
+    Args:
+        numbers (list): The set of numbers
+        size (int): Size of the subsets
+
+    Returns:
+        int: The sum of all unique sums
+
+    Example:
+        >>> get_sum({1, 3, 6, 8, 10, 11}, 3)
+        156
+    """
+    if reduce_array:
+        processed_numbers, ignored_numbers, extra = preprocess_data(numbers, size)
+    else:
+        processed_numbers, ignored_numbers, extra = numbers, [], []
+
+    iterables = [combinations(processed_numbers, size), extra]
+    sums, ignores = get_values(iterables, ignored_numbers)
+    unique_sums = sums.difference(ignores)
+    total_sum = sum(unique_sums)
+    return total_sum
+
+
+def get_values(iterables, force_ignores=[]):
+    """Calculate the sum of each subset.
+
+    Args:
+        iterables (`list` of `list`): Each iterable containing subsets to add
+        force_ignores (`list` of `int`): Each value on the list will cause all
+            groups with them on it to be excluded automatically, with the
+            exception of groups where all elements are in the exclude list.
+    Returns:
+        tuple: Two sets, one with the sums, the other with the duplicated ones
+    """
+    sums, ignores = set(), set()
+    for iterable in iterables:
+        for group in iterable:
+            value = sum(group)
+            if value in ignores:
+                continue
+            if value in sums:
+                ignores.add(value)
+                continue
+            sums.add(value)
+            # Force ignores
+            for number in force_ignores:
+                if number in group:
+                    # Check if the group contains only numbers to exclude
+                    for group_number in group:
+                        # Ignore group if one number is not in the exclude list
+                        if group_number not in force_ignores:
+                            ignores.add(value)
+                            break
+    return sums, ignores
+
+
+if __name__ == '__main__':  # pragma: no cover
+    numbers, size = [1, 3, 6, 8, 10, 11], 3
+    total = get_sum(numbers, size, True)
+    assert total == 156
+    print('Array: {}\nGroup Size: {}\nSum: {}'.format(numbers, size, total))
---
+++ hacker_rank/tests/subsets_with_a_unique_sum_test.py
@@ -0,0 +1,59 @@
+"""Tests for Project Euler #201: Subsets with a unique sum."""
+import unittest
+
+from hacker_rank.subsets_with_a_unique_sum import get_sum
+
+
+class TestUniqueSum(unittest.TestCase):
+
+    def test_website_example(self):
+        self.assertEqual(156, get_sum([1, 3, 6, 8, 10, 11], 3))
+
+    def test_array_with_only_one_element(self):
+        self.assertEqual(1, get_sum([1], 1))
+        self.assertEqual(2, get_sum([2], 1))
+
+    def test_n_equal_to_one(self):
+        self.assertEqual(10, get_sum([1, 3, 6], 1))
+        self.assertEqual(6, get_sum([3, 3, 6], 1))
+        self.assertEqual(0, get_sum([5, 5, 5], 1))
+
+    def test_n_equal_to_one_hundred(self):
+        self.assertEqual(500, get_sum([5]*100, 100))
+        self.assertEqual(0, get_sum([0]*100, 100))
+        self.assertEqual(5, get_sum([1] * 98 + [2, 3], 2))
+        self.assertEqual(0, get_sum([1, 2] + [3] * 98, 3))
+
+    def test_item_with_multiple_ocurrences(self):
+        self.assertEqual(0, get_sum([1, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3], 3))
+
+    def test_with_increasing_one_element(self):
+        self.assertEqual(76, get_sum([1, 3, 4, 6, 8], 3))
+        self.assertEqual(90, get_sum([1, 3, 4, 6, 8, 8], 3))
+        self.assertEqual(53, get_sum([1, 3, 4, 6, 8, 8, 8], 3))
+        self.assertEqual(29, get_sum([1, 3, 4, 6, 8, 8, 8, 8], 3))
+        self.assertEqual(29, get_sum([1, 3, 4, 6, 8, 8, 8, 8, 8], 3))
+        self.assertEqual(29, get_sum([1, 3, 4, 6, 8, 8, 8, 8, 8, 8], 3))
+
+    def test_increasing_group_size_with_duplicates(self):
+        numbers = [1, 3, 4, 6, 7, 7, 8, 8, 8, 9, 9, 9, 10, 10, 10, 10]
+        self.assertEqual(14, get_sum(numbers, 1))
+        self.assertEqual(9, get_sum(numbers, 2))
+        self.assertEqual(18, get_sum(numbers, 3))
+        self.assertEqual(54, get_sum(numbers, 4))
+        self.assertEqual(0, get_sum(numbers, 5))
+        self.assertEqual(28, get_sum(numbers, 6))
+        self.assertEqual(67, get_sum(numbers, 7))
+        self.assertEqual(0, get_sum(numbers, 8))
+        self.assertEqual(52, get_sum(numbers, 9))
+        self.assertEqual(91, get_sum(numbers, 10))
+        self.assertEqual(0, get_sum(numbers, 11))
+        self.assertEqual(184, get_sum(numbers, 12))
+        self.assertEqual(220, get_sum(numbers, 13))
+        self.assertEqual(229, get_sum(numbers, 14))
+        self.assertEqual(462, get_sum(numbers, 15))
+        self.assertEqual(119, get_sum(numbers, 16))
+
+
+if __name__ == '__main__':
+    unittest.main()
