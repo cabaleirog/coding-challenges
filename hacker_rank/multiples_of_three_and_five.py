c068740c244a8d548a82207f60b2efa19ed6cb1d
Paolo Cabaleiro <cabaleirog@gmail.com>
2018-01-15 14:30:31 -0300

    Adding a more generic implementation

--- hacker_rank/multiples_of_three_and_five.py
+++ hacker_rank/multiples_of_three_and_five.py
@@ -22,28 +22,70 @@ Find the sum of all the multiples of 3 or 5 below N.
 	For each test case, print an integer that denotes the sum of all the
     multiples of 3 or 5 below N.
 
-**Sample Input 0**
+**Sample Input**
 
     2
     10
     100
 
-**Sample Output 0**
+**Sample Output**
 
 	23
     2318
 
-**Explanation 0**
+**Explanation**
 
 	For N = 10, if we list all the natural numbers below 10 that are multiples
     of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
     Similarly for N = 100, we get 2318.
 """
-def get_sum(upper):
-    return sum(filter(lambda x: x % 3 == 0 or x % 5 == 0, range(1, upper)))
+def get_sum(upper_bound, values):
+    """Return the sum of all values divisables by a given set of values.
+
+    Args:
+        upper_bound (int): Number which is above all values to sum
+        values (list of int): List of the divisors to use
+
+    Returns:
+        int: Sum of all values
+    """
+    last_divisible = {}
+    # Get the last number below the upper bound for each value
+    i = upper_bound - 1
+    while True:
+        for value in values:
+            if i % value == 0 and value not in last_divisible:
+                last_divisible[value] = i
+        if len(last_divisible) == len(values):
+            break
+        i -= 1
+    return sum(set(get_divisors(values, last_divisible)))
+
+
+def get_divisors(divisors, last):
+    """Return the list of values divisible by the divisor.
+
+    Args:
+        divisor (int): Number to divide for
+        last (int): Last number of the sequence
+
+    Yields:
+        int: Values which are divisible by divisor
+    """
+    i = 0
+    divisors = sorted(divisors, reverse=True)
+    while divisors:
+        for divisor in divisors[:]:
+            number = last[divisor] - divisor * i
+            if number < divisor:
+                divisors.remove(divisor)
+                continue
+            yield number
+        i += 1
 
 
 if __name__ == '__main__':
-    assert get_sum(10) == 23
-    n = 1000
-    print('Sum of multiples of 3 or 5 below {} is {:,}'.format(n, get_sum(n)))
+    assert get_sum(10, [3, 5]) == 23
+    upper, numbers = 1000, [3, 5]
+    print('Sum of values divisible by {} below {} is {:,}'.format(
+        ', '.join(map(str, numbers)), upper, get_sum(upper, numbers)))
