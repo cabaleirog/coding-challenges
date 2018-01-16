17ea767244ff2e74f533206f07987f3b55161f4b
Paolo Cabaleiro <cabaleirog@gmail.com>
2018-01-16 02:04:18 -0300

    Completed the implementation and passed all the challenge tests

--- hacker_rank/multiples_of_three_and_five.py
+++ hacker_rank/multiples_of_three_and_five.py
@@ -39,7 +39,42 @@ Find the sum of all the multiples of 3 or 5 below N.
     of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
     Similarly for N = 100, we get 2318.
 """
-def get_sum(upper_bound, values):
+from itertools import combinations
+
+def get_summation(upper_bound, factors):
+    """Return the sum of all values divisables by a given set of values.
+
+    The summation of k from 1 to n is equal to
+
+        n * (n + 1) / 2
+
+    Thus, the sum of all multiples of X will be X times the summation.
+
+    Notes:
+        We have to account for duplicates and remove those extra values
+        Right shift has been used to avoid rounding issues on large numbers
+    """
+    upper_bound = int(upper_bound)  # Just in case the input is a float
+    total = 0
+
+    # Get the total sum for each factor
+    for factor in factors:
+        n = (upper_bound - 1) // factor
+        total += factor * n * (n + 1) >> 1 # Right shift 1 (Divided by 2)
+
+    # Subtract the sum for the overlapping values
+    # TODO: Only works for the case of 2 factors. Implement a generic approach
+    for pair in combinations(factors, 2):
+        factor = pair[0] * pair[1]
+        n = (upper_bound - 1) // factor
+        total -= factor * n * (n + 1) >> 1 # Right shift 1
+
+    return total
+
+
+# Below implementation is just for fun (Not using math to solve the problem)
+# This implementation works well for numbers below 10^6
+def get_sum(upper_bound, values, chunks=1):
     """Return the sum of all values divisables by a given set of values.
 
     Args:
@@ -48,6 +83,25 @@ def get_sum(upper_bound, values):
 
     Returns:
         int: Sum of all values
+    """
+    last_divisible = highest_multiple(upper_bound, values)
+    chunk_size = int(upper_bound / chunks)
+
+    total = 0
+    for value in multiples(values, last_divisible, chunk_size=chunk_size):
+        total += value
+    return total
+
+
+def highest_multiple(upper_bound, values):
+    """Return the last possible multiple below upper_bound for each factor
+
+    Args:
+        upper_bound (int): Number which is **above** all values to sum
+        values (list of int): List of the factors to use
+
+    Returns:
+        dict: Mapping of factor and highest multiple
     """
     last_divisible = {}
     # Get the last number below the upper bound for each value
@@ -59,14 +113,10 @@ def get_sum(upper_bound, values):
         if len(last_divisible) == len(values):
             break
         i -= 1
-
-    total = 0
-    for value in multiples(values, last_divisible):
-        total += value
-    return total
+    return last_divisible
 
 
-def multiples(factors_array, highest_multiples):
+def multiples(factors_array, highest_multiples, lowest=1, chunk_size=None):
     """Provide the list of integers which are divisible by any of the factors.
 
     Args:
@@ -76,13 +126,16 @@ def multiples(factors_array, highest_multiples):
     Yields:
         int: Values which are divisible by factor
     """
+    if chunk_size:
+        lowest = max(highest_multiples.values()) - chunk_size
+
     i = 0
     yielded = {x: {} for x in factors_array}  # Values which have been yielded
     factors = sorted(factors_array, reverse=True)
     while factors:
         for factor in factors[:]:
             number = highest_multiples[factor] - factor * i
-            if number < factor:
+            if number < factor or number < lowest:
                 factors.remove(factor)
                 continue
             # Check if we have already yielded a number
@@ -96,16 +149,27 @@ def multiples(factors_array, highest_multiples):
                 yielded[factor][number] = number
         i += 1
         # Clean already yielded
-        if i % 100 == 0:
+        if i % 100 == 0 and factors:
             cut_off = highest_multiples[min(factors)] - min(factors) * i
             # yielded will be cleaned completely for the smallest factor
             yielded[min(factors)] = {}
             for n in [x for x in yielded.keys() if x != min(factors)]:
                 yielded[n] = {k: v for k, v in yielded[n].items() if v < cut_off}
 
+    highest_multiples = highest_multiple(lowest, factors_array)
+    if max(highest_multiples.values()) <= 1:
+        return
+    yield from multiples(factors_array, highest_multiples, chunk_size=chunk_size)
+
 
 if __name__ == '__main__':
+    assert get_summation(10, [3, 5]) == 23
     assert get_sum(10, [3, 5]) == 23
-    upper, numbers = 1000, [3, 5]
-    print('Sum of values divisible by {} below {} is {:,}'.format(
-        ', '.join(map(str, numbers)), upper, get_sum(upper, numbers)))
+
+    upper, numbers = 10E5, [3, 5]
+    # Using the math approach
+    print('get_summation value for factors {} below {:.0f} is {:,.0f}'.format(
+        numbers, upper, get_summation(upper, numbers)))
+    # Using the recursive approach
+    print('get_sum value for factors {} below {:.0f} is {:,.0f}'.format(
+        numbers, upper, get_sum(upper, numbers, 20)))
--- hacker_rank/tests/multiples_of_three_and_five_test.py
+++ hacker_rank/tests/multiples_of_three_and_five_test.py
@@ -1,14 +1,32 @@
 """Tests for problem Multiples of 3 and 5."""
 import unittest
 
-from hacker_rank.multiples_of_three_and_five import get_divisors, get_sum
+from hacker_rank.multiples_of_three_and_five import (
+    multiples,
+    get_summation,
+    get_sum
+)
 
+
+class TestSummationOfMultiples(unittest.TestCase):
+    """TestCase for the get_summation method"""
+
+    def test_problem_example(self):
+        self.assertEqual(get_summation(10, [3, 5]), 23)
+        self.assertEqual(get_summation(100, [3, 5]), 2318)
+
+    def test_lowest_upper_bound(self):
+        self.assertEqual(get_summation(1, [3, 5]), 0)
+
+    def test_large_upper_bound(self):
+        self.assertEqual(get_summation(1000, [3, 5]), 233168)
 
 class TestSumOfMultiples(unittest.TestCase):
     """TestCase for the get_sum method"""
 
     def test_problem_example(self):
         self.assertEqual(get_sum(10, [3, 5]), 23)
+        self.assertEqual(get_sum(100, [3, 5]), 2318)
 
     def test_lowest_upper_bound(self):
         self.assertEqual(get_sum(1, [3, 5]), 0)
@@ -16,15 +34,22 @@ class TestSumOfMultiples(unittest.TestCase):
     def test_large_upper_bound(self):
         self.assertEqual(get_sum(1000, [3, 5]), 233168)
 
+    def test_chunks(self):
+        self.assertEqual(get_sum(1000, [3, 5]), 233168)
+        self.assertEqual(get_sum(1000, [3, 5]), 233168, 10)
+        self.assertEqual(get_sum(1000, [3, 5]), 233168, 20)
+        self.assertEqual(get_sum(1000, [3, 5]), 233168, 50)
+        self.assertEqual(get_sum(1000, [3, 5]), 233168, 500)
+
 
 class TestDividorsArray(unittest.TestCase):
     """TestCase for the get_divisors method"""
 
     def test_divisor_by_three(self):
-        self.assertEqual(set(get_divisors([3], {3: 15})), {3, 6, 9, 12, 15})
+        self.assertEqual(set(multiples([3], {3: 15})), {3, 6, 9, 12, 15})
 
     def test_divisor_by_five(self):
-        self.assertEqual(set(get_divisors([5], {5: 15})), {5, 10, 15})
+        self.assertEqual(set(multiples([5], {5: 15})), {5, 10, 15})
 
 
 if __name__ == '__main__':
