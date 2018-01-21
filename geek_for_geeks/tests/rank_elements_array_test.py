f59b69d805d088f56a71c7df91cf831caa34074c
Paolo Cabaleiro <cabaleirog@gmail.com>
2018-01-21 15:36:32 -0300

    Documented

--- geek_for_geeks/tests/rank_elements_array_test.py
+++ geek_for_geeks/tests/rank_elements_array_test.py
@@ -1,10 +1,10 @@
+"""TestCase for the Rank elements in an array challenge"""
 import unittest
 
 from geek_for_geeks.rank_elements_array import get_ranks
 
 
 class TestRankArrayElementsArray(unittest.TestCase):
-    """TestCase for the Rank elements in an array challenge"""
 
     def test_first_problem_example(self):
         values = [20, 30, 10]
@@ -16,10 +16,16 @@ class TestRankArrayElementsArray(unittest.TestCase):
         ranks = [1.5, 4.0, 6.0, 4.0, 1.5, 7.0, 4.0]
         self.assertEqual(get_ranks(values), ranks)
 
+    def test_third_problem_example(self):
+        values = [1, 2, 5, 2, 1, 60, 3]
+        ranks = [1.5, 3.5, 6.0, 3.5, 1.5, 7.0, 5.0]
+        self.assertEqual(get_ranks(values), ranks)
+
     def test_simple_sorted_list(self):
         values = [x for x in range(20)]
         ranks = [1.0 + float(x) for x in range(20)]
         self.assertEqual(get_ranks(values), ranks)
+
 
 if __name__ == '__main__':
     unittest.main()
