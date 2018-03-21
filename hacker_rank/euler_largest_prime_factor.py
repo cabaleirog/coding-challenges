2e05915cb273997c2e280d8b838b817c877b5a1d
Paolo Cabaleiro <cabaleirog@gmail.com>
2018-03-21 18:40:50 -0300

    Documentation updates and renaming functions

--- hacker_rank/euler_largest_prime_factor.py
+++ hacker_rank/euler_largest_prime_factor.py
@@ -5,20 +5,12 @@ https://www.hackerrank.com/contests/projecteuler/challenges/euler003
 The prime factors of 13195 are 5, 7, 13 and 29. What is the largest prime
 factor of the number N?
 
-Input Format:
-
-    First line contains T, the number of test cases. This is followed by T
-    lines each containing an integer N.
-
 Constraints:
-
     1 <= T <= 10
     10 <= N <= 10^12
-
-Output Format:
-
-    For each test case, display the largest prime factor of N.
 """
+
+
 def factorize(number):
     """Return the prime factors for a given number.
 
@@ -30,15 +22,13 @@ def factorize(number):
     """
     factors = set()
     aux = number
-    while True:
-        if aux == 1:
-            break
+    while aux != 1:
         if aux % 2 == 0:
             factors.add(2)
             aux = aux // 2
             continue
-        for i in range(3, aux + 1, 2):
-            if (aux / i) == (aux // i):
+        for i in range(3, aux + 1, 2):  # FIXME: Maybe until sqrt?
+            if aux / i == aux // i:
                 factors.add(i)
                 aux = aux // i
                 break
@@ -46,7 +36,9 @@ def factorize(number):
 
 
 if __name__ == '__main__':
-    target = 3 * 7 * 19 * 137 * 149
-    result = factorize(target)
-    print('Factors of {} are {}; the largest prime factor been {}'.format(
-        target, result, result[-1]))
+    print('!!!!!!!!!!!!!')
+    # target = 3 * 7 * 19 * 137 * 149
+    # result = factorize(target)
+    # print('Factors of {} are {}; the largest prime factor been {}'.format(
+    #     target, result, result[-1]))
+    print(factorize(int(10**12 + 1)))
--- hacker_rank/tests/climbing_the_leaderboard_test.py
+++ hacker_rank/tests/climbing_the_leaderboard_test.py
@@ -1,14 +1,16 @@
 from hacker_rank.climbing_the_leaderboard import climbing_leaderboard
 
 
-def test_ranks_first_after_playing():
+def test_alice_ranks_first_after_playing():
     scores = [100, 100, 50, 40, 40, 20, 10]
     alice = [5, 25, 50, 120]
     rank = [6, 4, 2, 1]
     assert list(climbing_leaderboard(scores, alice)) == rank
 
-def test_climbing_to_the_top_after_one_level():
+
+def test_alice_climbs_to_the_top_after_one_level():
     assert list(climbing_leaderboard([10, 5, 5, 5, 2], [15])) == [1]
+
 
 def test_alice_doesnt_play():
     assert list(climbing_leaderboard([5, 5, 3], [])) == []
