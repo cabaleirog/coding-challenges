39f0a4ad35839b4fe183560a6c265b4645417fbc
Paolo Cabaleiro <cabaleirog@gmail.com>
2018-05-04 02:27:21 -0300

    Fixing a bug when passing the args + documentation fixes

--- utils/test_utils.py
+++ utils/test_utils.py
@@ -9,9 +9,12 @@ def assert_time_limit(time_limit, func, *args, **kwargs):
         time_limit (int|float): max execution time allowed before test fails.
         func (obj): the function to evaluate.
 
-    Usage:
-        >>> args = [2, 4, 6, 8]
-        >>> assert_time_limit_multitest(5, foo, args)
+    Example:
+        >>> assert_time_limit(5, lambda x: x ** 2, 5)
+        >>> assert_time_limit(0.2, lambda x, y: x + y, 20, 30)
+        >>> import pytest
+        >>> with pytest.raises(AssertionError):
+        ...     assert_time_limit(0, lambda x: len(x), [2, 4, 6, 8])
     """
     start = time.perf_counter()
     _ = func(*args, **kwargs)
@@ -29,15 +32,27 @@ def assert_time_limit_multitest(time_limit, func, *args, **kwargs):
     Raises:
         ValueError: to avoid confusion with `assert_time_limit`.
 
-    Usage:
-        >>> args = [[2, 4, 6], [8, 8, 8], [2, 0, 10]]
-        >>> assert_time_limit_multitest(5, foo, args)
+    Example:
+        >>> import time, pytest
+        >>> def gpc(delay, char):
+        ...     for i in range(delay):
+        ...         time.sleep(0.1)
+        >>> assert_time_limit_multitest(0.8, gpc, (2, 'a'), (1, 'abc'))
+        >>> args = [(1, 'a'), (2, 'b'), (4, 'abc')]
+        >>> assert_time_limit_multitest(0.8, gpc, *args)
+        >>> with pytest.raises(AssertionError):
+        ...     assert_time_limit_multitest(0.6, gpc, *args)
     """
     if len(args) <= 1:
         raise ValueError(
-            'Multitest expects an iterable of size greater than one')
+            'Multitest expects two or more positional arguments')
     start = time.perf_counter()
     for test_args in args:
-        _ = func(test_args)
+        _ = func(*test_args, **kwargs)
     elapsed_time = time.perf_counter() - start
     assert elapsed_time < time_limit
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
