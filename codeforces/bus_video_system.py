3e4080919935cca61d4f827702e537493c08bac7
Paolo Cabaleiro <cabaleirog@gmail.com>
2018-05-20 23:42:14 -0400

    Docstring changes

--- codeforces/bus_video_system.py
+++ codeforces/bus_video_system.py
@@ -6,19 +6,20 @@ The buses in Berland are equipped with a video surveillance system. The
 system records information about changes in the number of passengers in a bus
 after stops.
 
-If ``x`` is the number of passengers in a bus just before the current bus
-stop and ``y`` is the number of passengers in the bus just after current bus
+If `x` is the number of passengers in a bus just before the current bus
+stop and `y` is the number of passengers in the bus just after current bus
 stop, the system records the number y - x. So the system records show how
 number of passengers changed.
 
-The test run was made for single bus and ``n`` bus stops. Thus, the system
+The test run was made for single bus and `n` bus stops. Thus, the system
 recorded the sequence of integers a[1], a[2], ..., a[n] (exactly one number
-for each bus stop), where a[i] is the record for the bus stop ``i``. The bus
+for each bus stop), where a[i] is the record for the bus stop `i`. The bus
 stops are numbered from 1 to n in chronological order.
 
 Determine the number of possible ways how many people could be in the bus
-before the first bus stop, if the bus has a capacity equals to ``w`` (that
+before the first bus stop, if the bus has a capacity equals to `w` (that
 is, at any time in the bus there should be from 0 to w passengers inclusive).
+
 """
 
 
